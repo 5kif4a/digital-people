dda8475793d3c2ba8bef816bde7089fed1c08bb5
5kif4a <theololo057@gmail.com>
2022-09-01 20:33:14 +0600

    fix: :bug: layout fixes

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -10,8 +10,8 @@ const Main = () => {
   return (
     <div className="main animate__animated animate__fadeIn">
       <div className="main__content">
-        <div></div>
-        
+        <div className="spacer"></div>
+
         <div className="text">
           <p dangerouslySetInnerHTML={{ __html: t("main_page_text") }} />
           <TGButton text={t("telegram")} />
@@ -26,61 +26,61 @@ const Main = () => {
         </div>
 
         <div className="info_wrapper">
-        <div className="info">
-          {i18n.language === "ru" ? (
-            <>
-              a{" "}
-              <Link className="link" to="detailed">
-                {t("here")}
-                <svg
-                  className="link__underline"
-                  width="263.78125"
-                  height="10"
-                  viewBox="0 0 263.78125 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    className="link__underline__path"
-                    d="M1,6 Q131.890625,-3 262.78125,3"
-                    stroke-width="2"
-                    stroke="black"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-dasharray="263.78125"
-                    stroke-dashoffset="263.78125"
-                  ></path>
-                </svg>
-              </Link>
-              {t("info_text")}
-            </>
-          ) : (
-            <>
-              {t("info_text")}{" "}
-              <Link className="link" to="detailed">
-                {t("here")}
-                <svg
-                  className="link__underline"
-                  width="263.78125"
-                  height="10"
-                  viewBox="0 0 263.78125 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    className="link__underline__path"
-                    d="M1,6 Q131.890625,-3 262.78125,3"
-                    stroke-width="2"
-                    stroke="black"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-dasharray="263.78125"
-                    stroke-dashoffset="263.78125"
-                  ></path>
-                </svg>
-              </Link>
-            </>
-          )}
+          <div className="info">
+            {i18n.language === "ru" ? (
+              <>
+                a{" "}
+                <Link className="link" to="detailed">
+                  {t("here")}
+                  <svg
+                    className="link__underline"
+                    width="263.78125"
+                    height="10"
+                    viewBox="0 0 263.78125 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      className="link__underline__path"
+                      d="M1,6 Q131.890625,-3 262.78125,3"
+                      stroke-width="2"
+                      stroke="black"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-dasharray="263.78125"
+                      stroke-dashoffset="263.78125"
+                    ></path>
+                  </svg>
+                </Link>
+                {t("info_text")}
+              </>
+            ) : (
+              <>
+                {t("info_text")}{" "}
+                <Link className="link" to="detailed">
+                  {t("here")}
+                  <svg
+                    className="link__underline"
+                    width="263.78125"
+                    height="10"
+                    viewBox="0 0 263.78125 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      className="link__underline__path"
+                      d="M1,6 Q131.890625,-3 262.78125,3"
+                      stroke-width="2"
+                      stroke="black"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-dasharray="263.78125"
+                      stroke-dashoffset="263.78125"
+                    ></path>
+                  </svg>
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
